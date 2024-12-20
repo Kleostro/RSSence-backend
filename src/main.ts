53029019ce4056e452b16378f7ed19c119990188
Max <133934232+Kleostro@users.noreply.github.com>
2024-12-20 15:20:24 +0300

    chore: setup prisma

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { IoAdapter } from '@nestjs/platform-socket.io';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new IoAdapter(app));
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('RSSence')
+    .setDescription('API documentation for RSSence')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();

56162a47eb6285c37943f8d96b6e2d1268de200c
Max <133934232+Kleostro@users.noreply.github.com>
2025-01-18 19:23:09 +0300

    fix: tests

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { PrismaService } from '../../prisma.service';
+import { UsersService } from '../users/users.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
@@ -9,7 +11,7 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [AuthService, PrismaService, UsersService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);

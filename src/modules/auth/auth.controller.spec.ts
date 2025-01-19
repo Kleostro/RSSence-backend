6d51a523147a83da035cc29281d016ea198dbbe8
Max <133934232+Kleostro@users.noreply.github.com>
2025-01-19 12:21:12 +0300

    fix: tests

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { PrismaService } from '../../prisma.service';
 import { UsersService } from '../users/users.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -10,7 +11,7 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService, UsersService],
+      providers: [AuthService, PrismaService, UsersService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { PrismaService } from '../../prisma.service';
 import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
 
@@ -8,7 +9,7 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService, UsersService],
+      providers: [AuthService, PrismaService, UsersService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);

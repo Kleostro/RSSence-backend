e421bc7003edd4bd58005daabbab187da5edb69c
Max <133934232+Kleostro@users.noreply.github.com>
2025-01-27 14:19:05 +0300

    feat: implement custom exceptions filter

---
+++ src/core/filters/exception.filter.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from 'express';
+
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  public catch(exception: unknown, host: ArgumentsHost): void {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    const statusCode = this.getStatus(exception);
+    const message = this.getMessage(exception);
+
+    response.status(statusCode).json({
+      statusCode,
+      path: request.url,
+      method: request.method,
+      timestamp: new Date().toISOString(),
+      message,
+    });
+  }
+
+  private getStatus(exception: unknown): number {
+    if (exception instanceof HttpException) {
+      return exception.getStatus();
+    }
+    return HttpStatus.INTERNAL_SERVER_ERROR;
+  }
+
+  private getMessage(exception: unknown): string {
+    if (exception instanceof HttpException) {
+      const response = exception.getResponse();
+      if (typeof response === 'object' && 'message' in response) {
+        if (typeof response.message === 'string') {
+          return response.message;
+        }
+        if (Array.isArray(response.message)) {
+          return response.message.join(', ');
+        }
+      }
+    }
+    return 'Server error';
+  }
+}

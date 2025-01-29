1ff7c96e82e8f478f438176743bb4204fb10fe0f
Max <133934232+Kleostro@users.noreply.github.com>
2025-01-29 16:43:49 +0300

    feat: cover profile controller with swagger

---
+++ src/modules/users/modules/profiles/profiles-controller-swagger.decorators.ts
@@ -0,0 +1,131 @@
+import { applyDecorators, HttpStatus } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
+
+import { CheckUsernameDto } from './dto/check-username';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
+
+export function ApiGetMeProfile(): MethodDecorator {
+  return applyDecorators(
+    ApiOperation({
+      summary: 'Get current profile',
+      description: 'Retrieve the current user profile.',
+    }),
+    ApiBearerAuth('jwt-access'),
+    ApiResponse({
+      status: HttpStatus.OK,
+      description: 'Returns the current user profile.',
+      type: CreateProfileDto,
+    }),
+    ApiResponse({
+      status: HttpStatus.NOT_FOUND,
+      description: 'Not found. The profile does not exist.',
+    }),
+    ApiResponse({
+      status: HttpStatus.UNAUTHORIZED,
+      description: 'Unauthorized. Invalid or expired access token.',
+    }),
+  );
+}
+
+export function ApiCreateMeProfile(): MethodDecorator {
+  return applyDecorators(
+    ApiOperation({
+      summary: 'Create a new profile',
+      description: 'Creates a new profile for the current user.',
+    }),
+    ApiBearerAuth('jwt-access'),
+    ApiBody({
+      required: false,
+      type: CreateProfileDto,
+    }),
+    ApiResponse({
+      status: HttpStatus.CREATED,
+      description: 'Returns the created profile.',
+      type: CreateProfileDto,
+    }),
+    ApiResponse({
+      status: HttpStatus.BAD_REQUEST,
+      description: 'Bad request. The data provided is invalid.',
+    }),
+    ApiResponse({
+      status: HttpStatus.CONFLICT,
+      description: 'Conflict. Attempting to reuse unique data.',
+    }),
+  );
+}
+
+export function ApiUpdateMeProfile(): MethodDecorator {
+  return applyDecorators(
+    ApiOperation({
+      summary: 'Update current profile',
+      description: 'Updates the current user profile.',
+    }),
+    ApiBearerAuth('jwt-access'),
+    ApiBody({
+      required: false,
+      type: UpdateProfileDto,
+    }),
+    ApiResponse({
+      status: HttpStatus.OK,
+      description: 'Returns the updated profile.',
+      type: UpdateProfileDto,
+    }),
+    ApiResponse({
+      status: HttpStatus.NOT_FOUND,
+      description: 'Not found. The profile does not exist.',
+    }),
+    ApiResponse({
+      status: HttpStatus.CONFLICT,
+      description: 'Conflict. Attempting to reuse unique data.',
+    }),
+    ApiResponse({
+      status: HttpStatus.UNAUTHORIZED,
+      description: 'Unauthorized. Invalid or expired access token.',
+    }),
+  );
+}
+
+export function ApiDeleteMeProfile(): MethodDecorator {
+  return applyDecorators(
+    ApiOperation({
+      summary: 'Delete current profile',
+      description: 'Deletes the current user profile.',
+    }),
+    ApiBearerAuth('jwt-access'),
+    ApiResponse({
+      status: HttpStatus.OK,
+      description: 'Successfully deleted the profile.',
+    }),
+    ApiResponse({
+      status: HttpStatus.NOT_FOUND,
+      description: 'Not found. The profile does not exist.',
+    }),
+    ApiResponse({
+      status: HttpStatus.UNAUTHORIZED,
+      description: 'Unauthorized. Invalid or expired access token.',
+    }),
+  );
+}
+
+export function ApiCheckAvailableUsername(): MethodDecorator {
+  return applyDecorators(
+    ApiOperation({
+      summary: 'Check if a username is available',
+      description: 'Check if a given username is already registered.',
+    }),
+    ApiBody({
+      required: true,
+      type: CheckUsernameDto,
+    }),
+    ApiResponse({
+      status: HttpStatus.OK,
+      description: 'Returns whether the username is available.',
+      schema: { type: 'boolean' },
+    }),
+    ApiResponse({
+      status: HttpStatus.BAD_REQUEST,
+      description: 'Bad request. The data provided is invalid.',
+    }),
+  );
+}

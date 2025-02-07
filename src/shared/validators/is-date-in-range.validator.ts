6a4df169909941e102083d0f15181fdb6c835ada
Max <133934232+Kleostro@users.noreply.github.com>
2025-02-07 20:18:13 +0300

    feat: implement data range falidator

---
+++ src/shared/validators/is-date-in-range.validator.ts
@@ -0,0 +1,38 @@
+import { registerDecorator, ValidationArguments, ValidationOptions } from 'class-validator';
+
+export function IsDateInRange(
+  { minDate, maxDate }: { minDate: Date | null; maxDate: Date | null },
+  validationOptions?: ValidationOptions,
+) {
+  return (object: object, propertyName: string): void => {
+    registerDecorator({
+      name: 'isDateInRange',
+      target: object.constructor,
+      propertyName,
+      constraints: [minDate, maxDate],
+      options: validationOptions,
+      validator: {
+        validate(value: Date, args: ValidationArguments & { constraints: Date[] }) {
+          const [min, max] = args.constraints;
+          if (!(value instanceof Date)) {
+            return false;
+          }
+
+          if (min && !max) {
+            return value >= min;
+          }
+
+          if (max && !min) {
+            return value <= max;
+          }
+
+          return value >= min && value <= max;
+        },
+        defaultMessage(args: ValidationArguments & { constraints: Date[] }) {
+          const [min, max]: Date[] = args.constraints;
+          return `Date must be between ${min.toISOString().split('T')[0]} and ${max.toISOString().split('T')[0]}`;
+        },
+      },
+    });
+  };
+}

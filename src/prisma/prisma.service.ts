47849c407821cc73e7f09758a0097e58b5e02cd2
Max <133934232+Kleostro@users.noreply.github.com>
2025-09-22 18:53:00 +0300

    feat: logging the prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,17 @@
 import { PrismaClient } from '@/generated/prisma';
 import { Global, Injectable, OnModuleInit } from '@nestjs/common';
 
+import { prismaMiddleware } from './prisma.middleware';
+
 @Global()
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super();
+
+    prismaMiddleware(this);
+  }
+
   public async onModuleInit(): Promise<void> {
     await this['$connect']();
   }
